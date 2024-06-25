fbbb17d480ad2a089b97966e8babf74d4a3bce38
kamilo9809 <camilorivera09@outlook.com>
2024-06-25 13:20:46 -0500

    feature/main/update_2/script_3

--- SCRIPT_2/projectAngular.sh
+++ SCRIPT_2/projectAngular.sh
@@ -7,6 +7,7 @@ echo "$direction"
 printf "Ingresa el nombre del proyecto (sin números): "
 read -r name
 
+# shellcheck disable=SC3010
 if [[ $name =~ [0-9] ]]; then
     echo "El nombre no debe contener números."
     exit 1
@@ -15,9 +16,11 @@ fi
 echo "Verificando que Angular CLI esté instalado..."
 
 ng --version >/dev/null 2>&1
+# shellcheck disable=SC2181
 if [ $? -ne 0 ]; then
     echo "Angular CLI no está instalado. Instalando la última versión..."
     npm install -g @angular/cli
+    # shellcheck disable=SC2181
     if [ $? -ne 0 ]; then
         echo "Hubo un error al intentar instalar Angular CLI. Por favor, verifica tu instalación de npm."
         exit 1
@@ -29,6 +32,7 @@ fi
 echo "Creando el proyecto Angular..."
 
 ng new "$name"
+# shellcheck disable=SC2181
 if [ $? -ne 0 ]; then
     echo "Hubo un error al crear el proyecto Angular."
     exit 1
@@ -41,6 +45,7 @@ cd "$name" || exit
 echo "Instalando dependencias del proyecto, esto puede tardar unos minutos..."
 
 npm i
+# shellcheck disable=SC2181
 if [ $? -ne 0 ]; then
     echo "Hubo un error al instalar las dependencias del proyecto."
     exit 1
